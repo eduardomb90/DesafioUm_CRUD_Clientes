fbbdd2894b5a9c54003ede389d85d0d256396592
emarquba <eduardo.marquesbarbosa@nttdata.com>
2021-11-12 14:44:14 -0300

    Adicionando cliente à lista

--- DesafioUm/Program.cs
+++ DesafioUm/Program.cs
@@ -6,10 +6,12 @@ namespace DesafioUm
     {
         static void Main(string[] args)
         {
-            IClienteService clientes = new ClienteService();
-            Cliente cliente = new ClienteFisico("Eduardo", "08969625470", new DateTime(1990,10,23), new Endereco("51020190", "Rua Rangel", "12", null, "Igreja de Belem", 
+            ClienteService clientes = new ClienteService();
+            Cliente cliente = new ClienteFisico("Eduardo", "08369525161", new DateTime(1990,10,23), new Endereco("51020190", "Rua Rangel", "12", null, "Igreja de Belem", 
                                                 "Recife", "PE"), "edu@email.com");
 
+            
+            clientes.AdicionarCliente(cliente);
             clientes.ListarClientes();
 
         }
